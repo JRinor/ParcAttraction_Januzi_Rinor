c0b89db397f0c6e4bbe1b5a03dc934f2a3f35e5c
JRinor <rinor.januzi7@etu.univ-lorraine.fr>
2025-02-07 14:35:45 +0100

    visible

--- python/sql_file/init.sql
+++ python/sql_file/init.sql
@@ -17,3 +17,22 @@ CREATE TABLE users (
     name varchar(255) not null,
     password varchar(255) not null
 );
+
+DROP TABLE IF EXISTS Notes;
+
+CREATE TABLE Notes(
+  notes_id int auto_increment,
+  primary key(notes_id),
+  text varchar(255), -- avis textuel 
+  etoiles int not null -- notes /5 etoiles
+);
+
+DROP TABLE IF EXISTS Note_Attraction;
+
+CREATE TABLE Note_Attraction(
+  notes_id int,
+  attraction_id int,
+  primary key(notes_id, attraction_id),
+  foreign key(notes_id) references Notes(notes_id),
+  foreign key(attraction_id) references attraction(attraction_id)
+);
